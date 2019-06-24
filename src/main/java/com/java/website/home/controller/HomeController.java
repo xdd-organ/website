037a48f00f78915025235bd2e3471d9a514a0368
xdd <767403510@qq.com>
2019-06-24 11:37:46 +0800

    上传日志配置

--- src/main/java/com/java/website/home/controller/HomeController.java
+++ src/main/java/com/java/website/home/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.java.website.home.controller;
 
 import com.java.website.home.mapper.HomeMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +20,20 @@ import java.util.Map;
 @RequestMapping("home")
 public class HomeController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     private HomeMapper homeMapper;
 
     @RequestMapping("home")
     public String home() {
+        LOGGER.info("HomeController.home");
         return "home";
     }
 
     @RequestMapping("index")
     public Object index() {
+        LOGGER.info("HomeController.index");
         List<Map<String, Object>> home = homeMapper.getHome();
         return home;
     }
